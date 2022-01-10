fb08e38a3225125bc4e1c95500fd65eccb7edcf5
kangkyung <rudgns0508@naver.com>
2022-01-10 09:12:32 +0900

    Chore: [#1]프로젝트 생성 및 패키징

---
+++ MisoWeather/MisoWeather/View/MisoWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MisoWeatherApp.swift
+//  MisoWeather
+//
+//  Created by 강경 on 2022/01/10.
+//
+
+import SwiftUI
+
+@main
+struct MisoWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
